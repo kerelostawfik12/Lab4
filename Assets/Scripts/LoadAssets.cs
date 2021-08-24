743567a84fed8920749fc4acfc0d9e750b58f3ee
kerelostawfik12 <53655688+kerelostawfik12@users.noreply.github.com>
2021-08-24 21:44:02 +1000

    100 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -10,8 +10,9 @@ public class LoadAssets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(redObj, new Vector3(2f, 0f, 0f), Quaternion.identity);
-        Instantiate(blueObj, new Vector3(-2f, 0f, 0f), Quaternion.identity);
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        //redObj.tag = "Red";
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
